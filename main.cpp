88bf04fd4cb2fc3666b5bc51e9ac19e78ab169ac
dolenko.aleksandr <dolenko.ayu@edu.spbstu.ru>
2025-11-10 12:56:11 +0300

    commit 5

--- main.cpp
+++ main.cpp
@@ -4,24 +4,33 @@ struct IntArray{
     void add(int i);
     int get(size_t id) const;
     size_t size() const;
+    int last() const;
+    IntArray(int i);
+    ~IntArray();
 }
 
 int main(){
     int next=0;
     std::cin>>next;
-   
-    IntArray a;
-    a.add(next);
-    while (std::cin>>next){
-        a.add(next);
-    }
-    if (std::cin.fail()){
-        return 1;
-    }
     
-    
-    
-    for(size_t i=0;i<a.size();++i){
-        int d=a.get(i);
+    try{
+        IntArray a(next);
+        while (std::cin>>next){
+            a.add(next);
+        }
+        if (std::cin.fail() && std::cin.eof()){
+            return 1;
+        }
+        
+        
+        size_t count=1;
+        for(size_t i=0;i<a.size()-1;++i){
+            int d=a.get(i);
+            count+=!(d % a.last())? 1 : 0;
+        }
     }
+    catch(const std::bad_alloc){
+        return 2;
+    }
+    std::cout<<count<<"\n";
 }
