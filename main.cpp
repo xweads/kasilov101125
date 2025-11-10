be6de62ceaa97d787b7894c482b46bc38ac87ad7
dolenko.aleksandr <dolenko.ayu@edu.spbstu.ru>
2025-11-10 12:39:21 +0300

    commit 3

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 
+struct IntArray{
+    void add(int i);
+    int get(size_t id) const;
+    size_t size() const;
+}
+
 int main(){
     int next=0;
     std::cin>>next;
     if (!std::cin){
         return 1;
+    }
+    IntArray a;
+    a.add(next);
+    
+    
+    
+    for(size_t i=0;i<a.size();++i){
+        int d=a.get(i);
     }
 }
